0afaddd936530c3d1c8590d665878edcce4ccbab
Lejla1502 <lejlasp@hotmail.com>
2022-12-17 13:53:55 +0100

    Setting up API to support XML

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -2,7 +2,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers().AddNewtonsoftJson(); //for PATCH method
+builder.Services.AddControllers(option =>
+{
+    //if the application type is not JSON, display error message
+    option.ReturnHttpNotAcceptable = true;
+    //if we define XML as return type in postman, it returns 406 - format type not acceptable
+
+    //but if we want API to support XML formatting, we add
+    //AddXmlDataContractSerializerFormatters - adds built in support for XML API
+    //and now output of the reqquest can be in XML format
+
+}).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters(); //for PATCH method
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
