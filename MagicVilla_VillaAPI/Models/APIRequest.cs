fd60b7fdfcb16d7e3291735eb8cfba384d0b638e
Lejla1502 <lejlasp@hotmail.com>
2022-12-23 17:47:28 +0100

    Adding API url in appsettings

--- MagicVilla_VillaAPI/Models/APIRequest.cs
+++ MagicVilla_VillaAPI/Models/APIRequest.cs
@@ -6,5 +6,7 @@ namespace MagicVilla_VillaAPI.Models
     public class APIRequest
     {
         public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public object Data { get; set; }
     }
 }
---
+++ MagicVilla_Web/MappingConfig.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.Dto;
+
+namespace MagicVilla_Web
+{
+    public class MappingConfig : Profile //Profile is inside the automapper
+    {
+        public MappingConfig() 
+        {
+            //ReverseMap means mapping goes both ways
+            //Villa to VillaDto and VillaDto to Villa
+            CreateMap<VillaDto, VillaCreateDto>().ReverseMap(); 
+            CreateMap<VillaDto, VillaUpdateDto>().ReverseMap();
+
+            //Villa Number
+            CreateMap<VillaNumberDto, VillaNumberCreateDto>().ReverseMap();
+            CreateMap<VillaNumberDto, VillaNumberUpdateDto>().ReverseMap();
+
+        }
+    }
+}
