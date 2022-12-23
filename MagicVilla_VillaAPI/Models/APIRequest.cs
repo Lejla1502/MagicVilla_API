4c13e4070348f9ee9754fa8a395abde8c8ecc410
Lejla1502 <lejlasp@hotmail.com>
2022-12-23 14:47:16 +0100

    Creating MVC project and adding API request

---
+++ MagicVilla_VillaAPI/Models/APIRequest.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static MagicVilla_Utility.StaticDetails;
+
+namespace MagicVilla_VillaAPI.Models
+{
+    public class APIRequest
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+    }
+}
