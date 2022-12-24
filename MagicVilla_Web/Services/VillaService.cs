48bb13355dc2cb8e4bf844ab496b16f10f16c337
Lejla1502 <lejlasp@hotmail.com>
2022-12-24 10:45:00 +0100

    Adding villa service to web project

---
+++ MagicVilla_Web/Services/VillaService.cs
@@ -0,0 +1,66 @@
+﻿using MagicVilla_Utility;
+using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.Dto;
+using MagicVilla_Web.Services.IServices;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.Extensions.Configuration;
+
+namespace MagicVilla_Web.Services
+{
+    public class VillaService : BaseService, IVillaService
+    {
+        private string villaUrl;
+        public VillaService(IHttpClientFactory httpClient, IConfiguration config) : base(httpClient)
+        {
+            //extracting api url from appsettings.json
+            villaUrl = config.GetValue<string>("ServiceUrls:VillaAPI");
+        }
+
+        public async Task<T> CreateAsync<T>(VillaCreateDto createDto)
+        {
+            return await SendAsync<T>(new APIRequest()
+            {
+                ApiType = StaticDetails.ApiType.POST,
+                Data = createDto,
+                Url = villaUrl + "/api/Villa"
+            });
+        }
+
+        public async Task<T> DeleteAsync<T>(int id)
+        {
+            return await SendAsync<T>(new APIRequest()
+            {
+                ApiType = StaticDetails.ApiType.DELETE,
+                Url = villaUrl + "/api/Villa/" + id
+            });
+        }
+
+        public async Task<T> GetAllAsync<T>()
+        {
+            return await SendAsync<T>(new APIRequest()
+            {
+                ApiType = StaticDetails.ApiType.GET,
+                Url = villaUrl + "/api/Villa"
+            });
+        }
+
+        public async Task<T> GetAsync<T>(int id)
+        {
+            return await SendAsync<T>(new APIRequest()
+            {
+                ApiType = StaticDetails.ApiType.GET,
+                Url = villaUrl + "/api/Villa/" + id
+            });
+        }
+
+        public async Task<T> UpdateAsync<T>(VillaUpdateDto updateDto)
+        {
+            return await SendAsync<T>(new APIRequest()
+            {
+                ApiType = StaticDetails.ApiType.PUT,
+                Data = updateDto,
+                Url = villaUrl + "/api/Villa/" + updateDto.Id
+            });
+        }
+    }
+}
