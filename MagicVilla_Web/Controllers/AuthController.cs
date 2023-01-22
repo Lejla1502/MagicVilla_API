25d8ce29aefe459dcff4c4c2e64b251c0ad7ab11
Lejla1502 <lejlasp@hotmail.com>
2023-01-22 01:34:50 +0100

    Implementing authentication controller

--- MagicVilla_Web/Controllers/AuthController.cs
+++ MagicVilla_Web/Controllers/AuthController.cs
@@ -1,8 +1,11 @@
 ﻿using AutoMapper;
+using MagicVilla_Utility;
 using MagicVilla_Web.Models;
 using MagicVilla_Web.Models.Dto;
 using MagicVilla_Web.Services.IServices;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace MagicVilla_Web.Controllers
 {
@@ -28,20 +31,24 @@ namespace MagicVilla_Web.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(LoginRequestDto dto)
         {
-            if (ModelState.IsValid)
+             APIResponse response = await _authService.LoginAsync<APIResponse>(dto);
+
+            if (response != null && response.IsSuccess)
             {
-                var response = await _authService.LoginAsync<APIResponse>(dto);
+                LoginResponseDto responseObj = JsonConvert.DeserializeObject<LoginResponseDto>(Convert.ToString(response.Result));
 
-                if (response != null && response.IsSuccess)
-                {
-                    TempData["success"] = "Login was successful!";
-                    return RedirectToAction("Login");
-                }
+                //we retrieve token from responseObj and store it in session
+                HttpContext.Session.SetString(StaticDetails.SessionToken, responseObj.Token);
+
+
+                TempData["success"] = "Login was successful!";
+                return RedirectToAction("Index", "Home");
             }
-
-            TempData["error"] = "Error encountered!!";
-
-            return View(dto);
+            else
+            {
+                ModelState.AddModelError("CustomError", response.ErrorMessages.FirstOrDefault());
+                return View(dto);
+            }
         }
 
         [HttpGet]
@@ -55,16 +62,15 @@ namespace MagicVilla_Web.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Register(RegistrationRequestDto dto)
         {
-            if (ModelState.IsValid)
-            {
-                var response = await _authService.RegisterAsync<APIResponse>(dto);
+            
+            var response = await _authService.RegisterAsync<APIResponse>(dto);
 
-                if (response != null && response.IsSuccess)
-                {
-                    TempData["success"] = "Registration was successful!";
-                    return RedirectToAction("Login");
-                }
+            if (response != null && response.IsSuccess)
+            {
+                TempData["success"] = "Registration was successful!";
+                return RedirectToAction("Login");
             }
+            
 
             TempData["error"] = "Error encountered!!";
 
@@ -73,7 +79,9 @@ namespace MagicVilla_Web.Controllers
 
         public async Task<IActionResult> Logout()
         {
-            return View();
+            await HttpContext.SignOutAsync();
+            HttpContext.Session.SetString(StaticDetails.SessionToken, "");
+            return RedirectToAction("Index", "Home");
         }
 
         public IActionResult AccessDenied()
