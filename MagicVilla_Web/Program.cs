59295132ef9f33cc5eab5ba3a8ce59986142598e
Lejla1502 <lejlasp@hotmail.com>
2023-01-23 00:16:43 +0100

    Adding login, logout, register to _Layout and registering HttpContextAccessor in DIC

--- MagicVilla_Web/Program.cs
+++ MagicVilla_Web/Program.cs
@@ -11,10 +11,11 @@ builder.Services.AddAutoMapper(typeof(MappingConfig));
 //adding httpClient on the villa service
 builder.Services.AddHttpClient<IVillaService, VillaService>();
 builder.Services.AddScoped<IVillaService, VillaService>();
-
 builder.Services.AddHttpClient<IVillaNumberService, VillaNumberService>();
 builder.Services.AddScoped<IVillaNumberService, VillaNumberService>();
 
+//to enable httpContext in Layout
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddHttpClient<IAuthService, AuthService>();    
 builder.Services.AddScoped<IAuthService, AuthService>();
 
