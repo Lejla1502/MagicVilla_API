21c4b234a70782c3b81106b9c224ebc7851d17f4
Lejla1502 <lejlasp@hotmail.com>
2023-05-14 22:41:56 +0200

    Bug fixes - switching userid from int to string and fixing jwt claim filtering

--- MagicVilla_Web/Controllers/AuthController.cs
+++ MagicVilla_Web/Controllers/AuthController.cs
@@ -47,7 +47,7 @@ namespace MagicVilla_Web.Controllers
 
                 //so the httpcontext knows that user is logged in - actually signing in a user
                 var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
-                identity.AddClaim(new Claim(ClaimTypes.Name, jwt.Claims.FirstOrDefault(u => u.Type == "name").Value));
+                identity.AddClaim(new Claim(ClaimTypes.Name, jwt.Claims.FirstOrDefault(u => u.Type == "unique_name").Value));
                 identity.AddClaim(new Claim(ClaimTypes.Role, jwt.Claims.FirstOrDefault(u=>u.Type=="role").Value));
                 var principal = new ClaimsPrincipal(identity);
                 await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
